c4b33b119a1025dcc0bbee7fd8a4179063ffe9e8
zhoushuai <dreamcoffeezs@163.com>
2016-05-11 19:15:48 +0800

    第一次提交

---
+++ ZST007_图片模糊/ZSTest/Resource/ThirdHelper/ImageBlur/UIImage+Blur.m
@@ -0,0 +1,136 @@
+//
+//  UIImage+Blur.m
+//  ZSTest
+//
+//  Created by zhoushuai on 16/5/11.
+//  Copyright © 2016年 zhoushuai. All rights reserved.
+//
+
+#import "UIImage+Blur.h"
+
+@implementation UIImage (Blur)
+
+
+
++(UIImage *)coreBlurImage:(UIImage *)image
+           withBlurNumber:(CGFloat)blur {
+
+    //获取图片资源
+    CIImage  *inputImage=[CIImage imageWithCGImage:image.CGImage];
+    //设置filter，高斯模糊的滤镜
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    //将图片输入到滤镜中
+    [filter setValue:inputImage forKey:kCIInputImageKey];
+    
+    //设置模糊程度,默认为10,取值范围(0-100)
+    [filter setValue:@(blur) forKey: @"inputRadius"];
+   
+    //将处理好的图片取出
+    CIImage *result=[filter valueForKey:kCIOutputImageKey];
+    
+    //context
+    CIContext *context = [CIContext contextWithOptions:nil];
+
+    //获取CGImage句柄,也就是从数据流中取出图片
+    CGImageRef outImage = [context createCGImage: result fromRect:[inputImage extent]];
+    //最终得到图片
+    UIImage *blurImage=[UIImage imageWithCGImage:outImage];
+    //释放CGImage句柄
+    CGImageRelease(outImage);
+    
+    return blurImage;
+}
+
+
+
+//加模糊效果，image是图片，blur是模糊度
++ (UIImage *)blurryImage:(UIImage *)image withBlurLevel:(CGFloat)blur
+{
+    if (image==nil)
+    {
+        NSLog(@"error:为图片添加模糊效果时，未能获取原始图片");
+        return nil;
+    }
+    //模糊度,
+    if ((blur < 0.1f) || (blur > 2.0f)) {
+        blur = 0.5f;
+    }
+    
+    //boxSize必须大于0
+    int boxSize = (int)(blur * 100);
+    boxSize -= (boxSize % 2) + 1;
+    NSLog(@"boxSize:%i",boxSize);
+    //图像处理
+    CGImageRef img = image.CGImage;
+    //需要引入#import <Accelerate/Accelerate.h>
+    /*
+     This document describes the Accelerate Framework, which contains C APIs for vector and matrix math, digital signal processing, large number handling, and image processing.
+     本文档介绍了Accelerate Framework，其中包含C语言应用程序接口（API）的向量和矩阵数学，数字信号处理，大量处理和图像处理。
+     */
+    
+    //图像缓存,输入缓存，输出缓存
+    vImage_Buffer inBuffer, outBuffer;
+    vImage_Error error;
+    //像素缓存
+    void *pixelBuffer;
+    
+    //数据源提供者，Defines an opaque type that supplies Quartz with data.
+    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
+    // provider’s data.
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    
+    //宽，高，字节/行，data
+    inBuffer.width = CGImageGetWidth(img);
+    inBuffer.height = CGImageGetHeight(img);
+    inBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
+    
+    //像数缓存，字节行*图片高
+    pixelBuffer = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    outBuffer.data = pixelBuffer;
+    outBuffer.width = CGImageGetWidth(img);
+    outBuffer.height = CGImageGetHeight(img);
+    outBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    // 第三个中间的缓存区,抗锯齿的效果
+    void *pixelBuffer2 = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    vImage_Buffer outBuffer2;
+    outBuffer2.data = pixelBuffer2;
+    outBuffer2.width = CGImageGetWidth(img);
+    outBuffer2.height = CGImageGetHeight(img);
+    outBuffer2.rowBytes = CGImageGetBytesPerRow(img);
+    //Convolves a region of interest within an ARGB8888 source image by an implicit M x N kernel that has the effect of a box filter.
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer2, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&outBuffer2, &inBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
+    }
+    //    NSLog(@"字节组成部分：%zu",CGImageGetBitsPerComponent(img));
+    //颜色空间DeviceRGB
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //用图片创建上下文,CGImageGetBitsPerComponent(img),7,8
+    CGContextRef ctx = CGBitmapContextCreate(
+                                             outBuffer.data,
+                                             outBuffer.width,
+                                             outBuffer.height,
+                                             8,
+                                             outBuffer.rowBytes,
+                                             colorSpace,
+                                             CGImageGetBitmapInfo(image.CGImage));
+    
+    //根据上下文，处理过的图片，重新组件
+    CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
+    UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    //clean up
+    CGContextRelease(ctx);
+    CGColorSpaceRelease(colorSpace);
+    free(pixelBuffer);
+    free(pixelBuffer2);
+    CFRelease(inBitmapData);
+    //CGColorSpaceRelease(colorSpace);   //多余的释放
+    CGImageRelease(imageRef);
+    return returnImage;
+}
+
+
+ @end
